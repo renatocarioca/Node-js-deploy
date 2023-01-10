360b89db2547096c9f2f9b153366bc26f0b2edd0
Renato Duarte <renato.duarte@online.uscs.edu.br>
2023-01-10 10:54:29 -0300

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('olá World')
 })
 
 app.listen(process.env.PORT || 3000)
